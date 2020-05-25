e581609809402634e2d1fabc7afa0d82cb151c6d
uedayusuke <uedayusuke1107@gmail.com>
2020-05-25 06:58:56 +0000

    [ADD]relations

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -3,4 +3,9 @@ class Shop < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :post_images, dependent: :destroy
+  has_many :contacts, dependent: :destroy
+  has_many :bookings, dependent: :destroy
+
 end
