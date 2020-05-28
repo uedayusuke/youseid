32d3a77b572973664bbfd4b2c37b01c85a0c1e4e
uedayusuke <uedayusuke1107@gmail.com>
2020-05-28 07:54:31 +0000

    [ADD]make_users/registrations#new

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,9 @@ class ApplicationController < ActionController::Base
 	before_action :configure_permitted_parameters, if: :devise_controller?
 
 	protected
+	#deviseのストロングパラメータ記述
 	def configure_permitted_parameters
 		devise_parameter_sanitizer.permit(:sign_up, keys:[:name, :phone_number, :postal_code, :address])
 	end
+
 end
