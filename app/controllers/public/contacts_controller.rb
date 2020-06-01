bad741e44cc8e8b239983df0989def1d8494c072
uedayusuke <uedayusuke1107@gmail.com>
2020-06-01 13:34:43 +0000

    [ADD]make_public/bookings#new

--- app/controllers/public/contacts_controller.rb
+++ app/controllers/public/contacts_controller.rb
@@ -4,16 +4,15 @@ class Public::ContactsController < ApplicationController
   	@contact = Contact.new
   end
 
-  def
-  	# ".create"でnewとsaveを同時に行う
-  	if Contact.create(create_params)
-  		#登録内容に不備がなければ問い合わせ完了ページにリダイレクト
-  		redirect_to finish_shop_contacts_path
-  	else
-  		#不備があった場合は問い合わせページから遷移しない
-  		render :new
-  	end
+  def create
+    #".create"でnew/saveを同時に行う
+    if Contact.create(create_params)
+      redirect_to finish_shop_contacts_path
+    else
+      render :new
+    end
   end
+
 
   def finish
   end
