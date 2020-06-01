a093e91b54af632af8a54de1e993fc4cd22f2307
uedayusuke <uedayusuke1107@gmail.com>
2020-06-01 07:53:58 +0000

    [ADD]make_admin/post_images#new

--- app/controllers/admin/post_images_controller.rb
+++ app/controllers/admin/post_images_controller.rb
@@ -1,16 +1,33 @@
 class Admin::PostImagesController < ApplicationController
   def new
+    @post_image = PostImage.new
   end
 
   def create
+    @post_image = PostImage.new(post_image_params)
+    #ログインしている人以外は投稿できない
+    @post_image.shop_id = current_shop.id
+    #登録内容に不備がある場合はrenderで新規投稿ページに戻る
+    if @post_image.save
+      redirect_to admin_post_image_path(@post_image.id)
+    else
+      render :new
+    end
   end
 
   def show
+    @post_image = PostImage.find(params[:id])
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+
+  def post_image_params
+    params.require(:post_image).permit(:title, :body, :image, :price)
   end
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '5ce58b5f99f0bd2a75bda7eff4b09cd9aa8686787596375fe28bce855890a9fb22c92077fb33db1cb7486b1638efd0f845cb6fae59e6cea56004c9af473201e8'
