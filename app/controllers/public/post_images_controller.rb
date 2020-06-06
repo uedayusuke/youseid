2fd19e3bcc37df5119e1ce69300698043005b1e8
uedayusuke <uedayusuke1107@gmail.com>
2020-06-06 04:15:24 +0000

    [ADD]コメント機能追加

--- app/controllers/public/post_images_controller.rb
+++ app/controllers/public/post_images_controller.rb
@@ -6,5 +6,6 @@ class Public::PostImagesController < ApplicationController
   def show
   	@post_image = PostImage.find(params[:id])
   	@reservation = Reservation.new
+  	@comment = Comment.new
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -29,7 +29,7 @@ Rails.application.routes.draw do
           get 'finish'
         end
       end
-      resource :comment, only: [:create]
+      resource :comments, only: [:create]
       resource :favorite, only: [:create, :destroy]
     end
 
