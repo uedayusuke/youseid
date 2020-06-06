5bc1ba30a519b724a32605d10fa3396ccbf74c5f
uedayusuke <uedayusuke1107@gmail.com>
2020-06-06 12:04:38 +0000

    [FIX]fix_public/bookings#new

--- app/controllers/admin/bookings_controller.rb
+++ app/controllers/admin/bookings_controller.rb
@@ -1,5 +1,6 @@
 class Admin::BookingsController < ApplicationController
   def index
+  	#whereを用いて、bookingに保存されているshop_idからログインしているshop.idを拾う
   	@bookings = Booking.where(shop_id: current_shop.id)
   end
 end
