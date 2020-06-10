599b319e57281548c34a03622eb32be331ec8258
uedayusuke <uedayusuke1107@gmail.com>
2020-06-10 04:53:06 +0000

    [ADD]add_validates_to_reservation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -9,6 +9,10 @@ class Reservation < ApplicationRecord
   with_options presence: true do |r|
   	r.validates :user_id
   	r.validates :post_image_id
+  	r.validates :title
+  	r.validates :user_name
+  	r.validates :user_email
+  	r.validates :shop_id
   end
 
 end
