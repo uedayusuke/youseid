3492cfce2e62999609afbac836d1d97a2a819b08
uedayusuke <uedayusuke1107@gmail.com>
2020-06-12 08:38:14 +0000

    [ADD]favorite_ajax

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -4,13 +4,13 @@ class Public::FavoritesController < ApplicationController
 		favorite = current_user.favorites.new(post_image_id: post_image.id)
 		favorite.save
 		#元いたページにリダイレクト
-		redirect_back(fallback_location: root_path)
+		#redirect_back(fallback_location: root_path)
 	end
 
 	def destroy
 		post_image = PostImage.find(params[:post_image_id])
 		favorite = current_user.favorites.find_by(post_image_id: post_image.id)
 		favorite.destroy
-		redirect_back(fallback_location: root_path)
+		#redirect_back(fallback_location: root_path)
 	end
 end
