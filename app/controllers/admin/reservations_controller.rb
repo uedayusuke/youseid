b16612771715b428011584c030c51f3349ea6326
uedayusuke <uedayusuke1107@gmail.com>
2020-06-13 12:53:23 +0000

    [FIX]add_order(created_at: :desc)

--- app/controllers/admin/reservations_controller.rb
+++ app/controllers/admin/reservations_controller.rb
@@ -1,6 +1,7 @@
 class Admin::ReservationsController < ApplicationController
   def index
   	#whereを用いて、reservationに保存されているshop_idからログインしているshop.idを拾う
-  	@reservations = Reservation.where(shop_id: current_shop.id)
+  	#.order(created_at: :desc)で新着順に
+  	@reservations = Reservation.where(shop_id: current_shop.id).order(created_at: :desc)
   end
 end
--- app/controllers/public/shops_controller.rb
+++ app/controllers/public/shops_controller.rb
@@ -1,5 +1,7 @@
 class Public::ShopsController < ApplicationController
   def show
   	@shop = Shop.find(params[:id])
+  	#.order(created_at: :desc)で新着順に
+  	@post_images = @shop.post_images.order(created_at: :desc)
   end
 end
