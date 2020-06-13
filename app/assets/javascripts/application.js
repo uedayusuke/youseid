d9bf8c95c99e751cf53345427b2a6cd7a37272c1
uedayusuke <uedayusuke1107@gmail.com>
2020-06-13 06:17:22 +0000

    [ADD]app/assets/javascripts/application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,7 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
-//= require_tree .
+
 
 //= require moment
 //= require moment/ja.js
@@ -22,3 +22,5 @@
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+
+//= require_tree .
