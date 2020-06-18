22b439125fdb2bb9a2efa444c63db99e117e216b
uedayusuke <uedayusuke1107@gmail.com>
2020-06-18 08:38:12 +0000

    [ADD]add_vision_api

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -5,6 +5,7 @@ class PostImage < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :reservations, dependent: :destroy
+  has_many :tags, dependent: :destroy
 
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
---
+++ app/models/tag.rb
@@ -0,0 +1,4 @@
+class Tag < ApplicationRecord
+	#relations
+	belongs_to :post_image
+end
